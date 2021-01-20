920b2c6c1d0cb999983c24515b7a2419e2588031
Ancilla Philip <ancilla.philip@gmail.com>
2021-01-20 13:54:26 +0530

    Activity Services

---
+++ MetropolisCityCorporation/demo2/service/IActivityService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demo2.Repo;
+using demo2.Models;
+using demo2.service.Dto;
+
+namespace demo2.service
+{
+    public interface IActivitydata
+    {
+        List<Models.Activities> Activity { get; }
+
+        Models.Activities GetActivity(Guid id);
+
+        Models.Activities AddActivity(Models.Activities activity);
+
+        void DeleteActivity(Models.Activities activity);
+
+        Models.Activities EditActivity(Models.Activities activity);
+        object GetActivities();
+    }
+
+}
