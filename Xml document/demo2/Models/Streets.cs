d52dc2255f6a31178b7828f0d78defd91d558155
jishnukj <jishnukjayan97@gmail.com>
2021-01-25 13:57:13 +0530

    Xml doc Streets

--- Xml document/demo2/Models/Streets.cs
+++ Xml document/demo2/Models/Streets.cs
@@ -6,16 +6,25 @@ using System.Threading.Tasks;
 
 namespace demo2.Models
 {
+    /// <summary>
+    /// The Streets class
+    /// containing all the Streets details.
+    /// </summary>
     public class Streets
     {
+        /// <value>Gets the Id</value>
         [Key]
         public int Id { get; set; }
+        /// <value>Gets the Name</value>
         [Required]
         public string Name { get; set; }
+        /// <value>Gets the Location</value>
         [Required]
         public string Location { get; set; }
+        /// <value>Gets the Isclosed</value>
         [Required]
         public bool IsClosed { get; set; }
+        /// <value>Gets the IsSingleLane</value>
         [Required]
         public bool IsSingleLane { get; set; }
     }
