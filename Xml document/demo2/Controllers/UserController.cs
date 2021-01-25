9669021f3375a9ac81a09bcb3d0f8602815e4266
Joshua Philip <josh.gtra@gmail.com>
2021-01-25 20:57:15 +0530

    User Controller

--- Xml document/demo2/Controllers/UserController.cs
+++ Xml document/demo2/Controllers/UserController.cs
@@ -7,23 +7,40 @@ using demo2.service;
 
 namespace demo2.Controllers
 {
+    /// <summary>
+    /// The main User Controller which contains 
+    /// all the methods for the API endpoints related to User data
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
-        private IUserdata _userdata;
 
+        private IUserdata _userdata;
+        /// <summary>
+        /// User Controller Constructor
+        /// </summary>
+        /// <param name="userdata">userdata object which
+        /// inherits from IUserdata interface</param>
         public UserController(IUserdata userdata)
         {
             _userdata = userdata;
         }
+        /// <summary>
+        /// GetUsers Method for getting all user details
+        /// </summary>
+        /// <returns>All user details</returns>
         [HttpGet]
         [Route("api/[controller]")]
         public IActionResult GetUsers()
         {
             return Ok(_userdata.GetUsers());
         }
-
+        /// <summary>
+        /// GetUser Method for fetching details of a single user based on id.
+        /// </summary>
+        /// <param name="id">Globally Unique User id defined in User Entity</param>
+        /// <returns>User with id specified</returns>
         [HttpGet]
         [Route("api/[[controller]]/[[id]]")]
         public IActionResult GetUser(Guid id)
