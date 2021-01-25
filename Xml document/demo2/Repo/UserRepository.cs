dba08294fbc246bffd2372be4f100cb604b56026
Resmita <resmita34@gmail.com>
2021-01-25 19:04:45 +0530

    User File with xml documentation

--- Xml document/demo2/Repo/UserRepository.cs
+++ Xml document/demo2/Repo/UserRepository.cs
@@ -6,13 +6,27 @@ using demo2.Models;
 
 namespace demo2.Repo
 {
+    /// <summary>
+    /// User Class
+    /// </summary>
     public class UserRepository : IUserRepository
     {
+        /// <summary>
+        /// Create a readonly Object
+        /// </summary>
         private readonly ActivityContext _dbContext;
+        /// <summary>
+        /// Constructor for Activity Context Class
+        /// </summary>
+        /// <param name="dbContext">dbContext</param>
         public UserRepository(ActivityContext dbContext)
         {
             _dbContext = dbContext;
         }
+        /// <summary>
+        /// Get a list of Users
+        /// </summary>
+        /// <returns>returns list of users</returns>
         public List<Users> GetAllUsers()
         {
             return _dbContext.Users.ToList();
