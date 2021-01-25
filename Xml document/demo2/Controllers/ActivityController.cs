8332c0a677242575926cbcb6858f4331c7d15e04
Joshua Philip <josh.gtra@gmail.com>
2021-01-25 20:55:57 +0530

    Activity Controller

--- Xml document/demo2/Controllers/ActivityController.cs
+++ Xml document/demo2/Controllers/ActivityController.cs
@@ -7,22 +7,40 @@ using demo2.service;
 
 namespace demo2.Controllers
 {
+ /// <summary>
+ /// The main activities Controller Class
+ /// which contains all the methods for the API endpoints 
+ /// related to Activity Data.
+ /// </summary>
     [ApiController]
     public class ActivitesController : ControllerBase
     {
         private IActivitydata _activitydata;
 
+     /// <summary>
+     /// Activities Controller constructor.
+     /// </summary>
+     /// <param name="activitydata">activitydata object which 
+     /// inherits from IActivitydata interface</param>
         public ActivitesController(IActivitydata activitydata)
         {
             _activitydata = activitydata;
         }
+    /// <summary>
+    /// GetActivities Method for getting all activity details
+    /// </summary>
+    /// <returns>All activity details</returns>
         [HttpGet]
         [Route("api/[[controller]]")]
         public IActionResult GetActivities()
         {
             return Ok(_activitydata.GetActivities());
         }
-
+    /// <summary>
+    /// GetActivity Method for fetching details of a single activity based on ID
+    /// </summary>
+    /// <param name="id">Globally Unique ID defined in Activity Entity </param>
+    /// <returns>Activity with ID specified by user</returns>
         [HttpGet]
         [Route("api/[[controller]]/[[id]]")]
         public IActionResult GetActivity(Guid id)
@@ -35,17 +53,27 @@ namespace demo2.Controllers
             }
             return NotFound($"Activity with Id: {id} was not found");
         }
+    /// <summary>
+    /// AddActivity Method for adding activity details.
+    /// </summary>
+    /// <param name="activity">The activity object derived from Models.activities</param>
+    /// <returns>The newly created object</returns>
 
         [HttpPost]
         [Route("api/[[controller]]/[[id]]")]
-        public IActionResult GetActivity(Models.Activities activity)
+        public IActionResult AddActivity(Models.Activities activity)
         {
             _activitydata.AddActivity(activity);
 
             return Created(HttpContext.Request.Scheme + "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" +
                 activity.Id, activity);
         }
-
+    /// <summary>
+    /// DeleteActivity Method for deleting activity details.
+    /// </summary>
+    /// <param name="id">Globally unique ID defined in Activity Entity</param>
+    /// <returns>An empty object if object is found or
+    /// else a string containing the error message</returns>
         [HttpDelete]
         [Route("api/[[controller]]/[[id]]")]
         public IActionResult DeleteActivity(Guid id)
@@ -59,7 +87,13 @@ namespace demo2.Controllers
             }
             return NotFound($"Activity with Id: {id} was not found");
         }
-
+    /// <summary>
+    /// Edit Activity Method for editing activity details.
+    /// </summary>
+    /// <param name="id">Globally Unique ID defined in Activity Entity</param>
+    /// <param name="activity">The activity object derived 
+    /// from Models.activities</param>
+    /// <returns>The activity details which are edited</returns>
         [HttpPatch]
         [Route("api/[[controller]]/[[id]]")]
         public IActionResult EditActivity(Guid id, Models.Activities activity)
