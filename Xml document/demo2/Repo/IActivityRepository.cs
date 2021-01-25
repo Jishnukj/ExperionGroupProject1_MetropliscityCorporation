bc7592e1de319e9d2d4e1f3c20ed2d4e1eb2b169
Resmita <resmita34@gmail.com>
2021-01-25 19:01:15 +0530

    Activity Interface with xml documentation

--- Xml document/demo2/Repo/IActivityRepository.cs
+++ Xml document/demo2/Repo/IActivityRepository.cs
@@ -6,12 +6,30 @@ using demo2.Models;
 
 namespace demo2.Repo
 {
+    /// <summary>
+    /// Creating an Interface
+    /// </summary>
     public interface IActivityRepository
     {
+        /// <summary>
+        /// Get a list of Activities
+        /// </summary>
         List<Activities> GetAllActivities();
+        /// <summary>
+        /// Add a list of Activities
+        /// </summary>
         void Add(Activities itemEntity);
+        /// <summary>
+        /// Update a list of Activities
+        /// </summary>
         Activities Update(Activities itemChange, int id);
+        /// <summary>
+        /// Delete a list of Activities by Id
+        /// </summary>
         bool Delete(int id);
+        /// <summary>
+        /// Get the list of Activities after edits
+        /// </summary>
         List<Activities> GetActivities();
 
     }
