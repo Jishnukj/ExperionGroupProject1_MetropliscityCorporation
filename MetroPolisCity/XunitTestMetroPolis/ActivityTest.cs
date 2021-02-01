511577e029ce3fe5248672ec35f6193fceb088f4
Jishnukj <jishnukjayan97@gmail.com>
2021-02-01 18:35:27 +0530

    activitytest

--- MetroPolisCity/XunitTestMetroPolis/ActivityTest.cs
+++ MetroPolisCity/XunitTestMetroPolis/ActivityTest.cs
@@ -8,47 +8,80 @@ using MetroPolisDataService.Entities;
 using System.Collections.Generic;
 using MetroPolisBusinessService.Dto;
 using System.Linq;
+using Moq;
+using FluentAssertions;
 
 namespace XunitTestMetroPolis
 {
     public class ActivityTest
     {
+        private Mock<IActivityRepo> mockActivityRepo = new Mock<IActivityRepo>();
+
         [Fact]
         public void GetallActivityValid()
         {
-            using (var mock = AutoMock.GetLoose())
-            {
-                mock.Mock<IActivityRepo>()
-                    .Setup(x => x.GetAllActivities())
-                    .Returns(GetSamples());
-                var cls = mock.Create<ActivityService>();
-                var expected = GetSamples();
-                var actual = cls.GetActivities();
-                Assert.True(actual != null);
-                Assert.Equal(expected.Count, actual.Count);
-                Assert.Equal(expected[1].Name, actual[1].Name);
-            }
+            mockActivityRepo.Setup(x => x.GetAllActivities())
+                .Returns(GetSamples());
+            var cls = new ActivityService(mockActivityRepo.Object);
+            var expected = GetSamples();
+            var actual = cls.GetActivities();
+            Assert.True(actual != null);
+            Assert.Equal(expected.Count, actual.Count);
+            Assert.Equal(expected[1].Name, actual[1].Name);
         }
         [Fact]
         public void GetActivityValid()
         {
-            using (var mock = AutoMock.GetLoose())
+            int id = 2;
+            var activity = GetSamples()[id];
+            mockActivityRepo.Setup(x => x.GetActivity(It.IsAny<int>()))
+                .Returns(activity);
+            var cls = new ActivityService(mockActivityRepo.Object);
+            var expected = GetSamples().FirstOrDefault(e => e.Id == id);
+            var actual = cls.GetActivity(id);
+            Assert.True(actual != null);
+            Assert.Equal(expected.StreetName, actual.StreetName);
+            Assert.Equal(expected.Name, actual.Name);
+        }
+        [Fact]
+        public void Delete_WithValidId_ReturnTrue()
+        {
+            var activity = new Activity
             {
-                int id = 2;
-                mock.Mock<IActivityRepo>()
-                    .Setup(x => x.GetActivity(id))
-                    .Returns(GetSamples().Where(e => e.Id == id).FirstOrDefault());
-                var cls = mock.Create<ActivityService>();
-                var expected = GetSamples().Where(e => e.Id == id).FirstOrDefault();
-                var actual = cls.GetActivity(id);
-                Assert.True(actual != null);
-                Assert.Equal(expected.StreetName, actual.StreetName);
-                Assert.Equal(expected.Name, actual.Name);
-            }
+                Id = 1,
+                Date = DateTime.Now
+            };
+            var deleteResponse = true;
+            mockActivityRepo.Setup(x => x.GetActivity(It.IsAny<int>()))
+                .Returns(activity);
+            mockActivityRepo.Setup(x => x.Delete(It.IsAny<int>()))
+                .Returns(deleteResponse);
+            var service = new ActivityService(mockActivityRepo.Object);
+            //Act
+            var result = service.Delete(1);
+            //Assert
+            result.Should().Be(deleteResponse);
+
+        }
+        [Fact]
+        public void Delete_WithInvalidId_ReturnFalse()
+        {
+            var activity = new Activity
+            {
+                Id = 1,
+                Date = DateTime.Now
+            };
+            var deleteResponse = false;
+            mockActivityRepo.Setup(x => x.Delete(It.IsAny<int>()))
+                .Returns(deleteResponse);
+            var service = new ActivityService(mockActivityRepo.Object);
+            //Act
+            var result = service.Delete(1);
+            //Assert
+            result.Should().Be(deleteResponse);
+
         }
 
-
-       
 
         private List<Activity> GetSamples()
         {
